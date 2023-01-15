5e6c65e64dd484e44a3ccae88202bab9adfd8b0f
chandanRaj <raj.chandan91@gmail.com>
2023-01-15 15:18:40 +0530

    Meal structuring

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { Fragment } from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 function App() {
   return (
-    <Fragment><Header></Header></Fragment>
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+      </Fragment>
   );
 }
 
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = () => {
+    return <Fragment>
+        <MealsSummary />
+        <AvailableMeals />
+    </Fragment>
+};
+export default Meals;
