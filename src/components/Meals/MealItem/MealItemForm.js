9a62ebb2995250bc7d1c90f9e42ac80f59977a5b
chandanRaj <raj.chandan91@gmail.com>
2023-01-22 21:42:07 +0530

    mealitem form

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,10 @@
+import classes from "./MealItemForm.module.css";
+const MealItemForm = (props) =>
+{
+    return <form className={classes.form}>
+        <input />
+        <button> Add + </button>
+    </form>
+}
+
+export default MealItemForm;
