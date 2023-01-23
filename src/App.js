14418203e1747f7548b221a2d606f053982de272
chandanRaj <raj.chandan91@gmail.com>
2023-01-23 22:52:18 +0530

    state handler

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import { Fragment } from "react";
+import { Fragment,useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 function App() {
+  const [cartIsShown,setCartIsShown] = useState(false);
+
+  const showCartHandler = () =>
+  {
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler = () =>
+  {
+    setCartIsShown(false);
+  }
+
   return (
     <Fragment>
       <Cart/>
