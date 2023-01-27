379b96674398e83e823315ead598bf67ae2e4040
chandanRaj <raj.chandan91@gmail.com>
2023-01-27 23:43:34 +0530

    cart context

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,6 +1,18 @@
+import CartContext from "./cart-context";
+
 const CartProvider = (props) => 
 {
-
+    const addItemToCartHandler = (item) => {};
+    const removeItemFromCartHandler = (item) => {};
+    const cartContext = {
+        items : [],
+        totalAmount : 0,
+        addItem : addItemToCartHandler,
+        removeItem : removeItemFromCartHandler
+    }
+    return <CartContext.Provider value={cartContext}>
+        {props.children}
+    </CartContext.Provider>
 }
 
 export default CartProvider;
